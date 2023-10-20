69a9ea973484644702378732218c2c1a73aab333
Tommy Nguyen <tommynguyen.164@gmail.com>
2023-10-19 22:30:05 -0700

    Create group and member table migrations

---
+++ groups/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from users.models import User
+
+
+class Group(models.Model):
+    class Schedule(models.TextChoices):
+        BIWEEKLY = "BIWEEKLY"
+        QUARTERLY = "QUARTERLY"
+        MONTHLY = "MONTHLY"
+
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, blank=False)
+    active = models.BooleanField(default=True)
+    schedule = models.CharField(choices=Schedule.choices, default=Schedule.BIWEEKLY)
+    logo_url = models.TextField(blank=True, null=True)
+    last_issue_date = models.DateField(blank=True, null=True)
+    users = models.ManyToManyField(User, through="Member")
+
+    def __str__(self):
+        return self.name
+
+
+class Member(models.Model):
+    class Roles(models.TextChoices):
+        ADMIN = "ADMIN"
+        MEMBER = "MEMBER"
+        OWNER = "OWNER"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    role = models.CharField(choices=Roles.choices, blank=False)
+
+    def __str__(self):
+        return f"{self.user}, {self.group}, {self.role}"
