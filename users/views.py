08cecc32af5ac8989ab9ebb234e6631b9687a9b7
Tommy Nguyen <tommynguyen.164@gmail.com>
2024-01-02 20:11:56 -0800

    Update view to only include single-instance methods retrieve/update/delete

--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
-from rest_framework import permissions, viewsets
+from rest_framework import mixins, permissions, viewsets
 
 from users.models import User
 from users.serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
