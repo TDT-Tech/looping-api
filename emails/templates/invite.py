d191b68c73f774e0cc2314152025d601e4cffc8f
Tommy Nguyen <tommynguyen.164@gmail.com>
2024-02-01 22:13:21 -0800

    Create initial templates for invite, newsletter and reply emails

---
+++ emails/templates/invite.py
@@ -0,0 +1,184 @@
+INVITE_OUTLINE = """
+<div style="margin:0;padding:0;background-color:#ffffff">
+  <table style="table-layout:fixed;vertical-align:top;min-width:320px;Margin:0 auto;border-spacing:0;border-collapse:collapse;background-color:#ffffff;width:100%" cellpadding="0" cellspacing="0" role="presentation" width="100%" bgcolor="#ffffff" valign="top">
+    <tbody>
+      <tr style="vertical-align:top" valign="top">
+        <td style="word-break:break-word;vertical-align:top" valign="top">
+          <div style="background-color:transparent">
+            <div class="m_-9144213743796713562block-grid" style="Margin:0 auto;min-width:320px;max-width:500px;word-wrap:break-word;word-break:break-word;background-color:transparent">
+              <div style="border-collapse:collapse;display:table;width:100%;background-color:transparent">
+                <div class="m_-9144213743796713562col" style="min-width:320px;max-width:500px;display:table-cell;vertical-align:top;width:500px">
+                  <div style="width:100%!important">
+                    <div style="border-top:0px solid transparent;border-left:0px solid transparent;border-bottom:0px solid transparent;border-right:0px solid transparent;padding-top:40px;padding-bottom:15px;padding-right:0px;padding-left:0px">
+                      <!-- TODO LOGO URL -->
+                      <div align="center" style="padding-right:0px;padding-left:0px">
+                        <img align="center" border="0" src={LOGO_URL} style="text-decoration:none;height:auto;border:0;width:100%;max-width:50px;display:block;border-radius:50%" width="50">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div style="background-color:transparent">
+            <div class="m_-9144213743796713562block-grid" style="Margin:0 auto;min-width:320px;max-width:500px;word-wrap:break-word;word-break:break-word;background-color:transparent">
+              <div style="border-collapse:collapse;display:table;width:100%;background-color:transparent"> 
+                <div class="m_-9144213743796713562col" style="min-width:320px;max-width:500px;display:table-cell;vertical-align:top;width:500px">
+                  <div style="width:100%!important">
+                    <div style="border-top:0px solid transparent;border-left:0px solid transparent;border-bottom:0px solid transparent;border-right:0px solid transparent;padding-top:20px;padding-bottom:20px;padding-right:0px;padding-left:0px">
+                      <div style="color:#555555;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;line-height:1.5;padding-top:10px;padding-right:0px;padding-bottom:5px;padding-left:0px">
+                        <div style="line-height:1.5;font-size:12px;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;color:#555555">
+                          <p style="font-size:14px;line-height:1.5;word-break:break-word;text-align:center;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;margin:0"><strong><span style="color:#000000"><span style="font-size:24px">Welcome to Looping!&nbsp;</span></span></strong></p>
+                        </div>
+                      </div>
+                      <!-- TODO ADMIN USER AND GROUP NAME -->
+                      <div style="color:#555555;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;line-height:1.5;padding-top:10px;padding-right:0px;padding-bottom:5px;padding-left:0px">
+                        <div style="line-height:1.5;font-size:12px;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;color:#555555">
+                          <p style="font-size:20px;line-height:1.5;word-break:break-word;text-align:center;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;margin:0"><span style="font-size:20px"><span style="color:#000000"><strong>{ADMIN_NAME}</strong> started a group newsletter called <em><strong>{GROUP_NAME}</strong></em>&nbsp;with you, where everyone asks and answers questions about their lives.&nbsp;</span></span></p>
+                        </div>
+                      </div>
+                      
+                      <div align="center" style="padding-right:0px;padding-left:0px">
+                        
+                        <div style="font-size:1px;line-height:35px">&nbsp;</div>
+                        
+                      </div>
+                      <!-- TODO REPLY BUTTON URL -->
+                      <div align="center" style="padding-top:10px;padding-right:0px;padding-bottom:10px;padding-left:0px">
+                        <a href={REPLY_URL} style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#56a778;border-radius:4px;width:auto;width:auto;border-top:1px solid #56a778;border-right:1px solid #56a778;border-bottom:1px solid #56a778;border-left:1px solid #56a778;padding-top:10px;padding-bottom:10px;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;text-align:center;word-break:keep-all" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://letterloop.co&amp;source=gmail&amp;ust=1601516355513000&amp;usg=AFQjCNEMbaGBkj4D9XDyT0KSdvB7AB5oeA"><span style="padding-left:50px;padding-right:50px;font-size:16px;display:inline-block"><span style="font-size:16px;line-height:2;word-break:break-word"><strong>Add your reply&nbsp; ➝</strong></span></span></a>
+                        
+                      </div>
+                      
+                      <!-- TODO ISSUE DATE -->
+                      <div style="color: #555555; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; line-height: 1.8; padding-top: 10px; padding-right: 10px; padding-left: 10px;">
+                      <div style="line-height: 1.8; font-size: 12px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; color: #555555;">
+                          <p style="font-size: 15px; line-height: 1.8; word-break: break-word; text-align: center; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; margin: 0;">
+                              <span style="font-size: 15px; color: #000000;">Reply by {ISSUE_DATE}</span>
+                          </p>
+                      </div>
+                      </div>
+                      
+                    </div>
+                    
+                  </div>
+                </div>
+                
+                
+              </div>
+            </div>
+          </div>
+          <!-- TODO HOW IT WORKS SECTION, ADMIN NAME -->
+          <div style="background-color:transparent">
+            <div class="m_-9144213743796713562block-grid" style="Margin:0 auto;min-width:320px;max-width:500px;word-wrap:break-word;word-break:break-word;background-color:#ffffff">
+              <div style="border-collapse:collapse;display:table;width:100%;background-color:#ffffff">
+                
+                
+                <div class="m_-9144213743796713562col" style="min-width:320px;max-width:500px;display:table-cell;vertical-align:top;width:500px">
+                  <div style="width:100%!important">
+                    <div style="border-top:0px solid transparent;border-left:0px solid transparent;border-bottom:0px solid transparent;border-right:0px solid transparent;padding-top:5px;padding-bottom:5px;padding-right:0px;padding-left:0px">
+                      <div style="color:#555555;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;line-height:1.8;padding-top:0px;padding-right:35px;padding-bottom:30px;padding-left:35px">
+                        <div style="line-height:1.8;font-size:12px;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;color:#555555">
+                          <p style="line-height:1.8;word-break:break-word;text-align:center;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;font-size:22px;margin:0"><span style="color:#000000;font-size:22px"><strong><span>How it Works</span></strong></span></p>
+                        </div>
+                      </div>
+                      
+                      <div style="font-size:16px;text-align:center;font-family:Arial,Helvetica Neue,Helvetica,sans-serif">
+                        <div>
+                          <img src="https://ucarecdn.com/98f1a1d8-293d-49fb-9250-574ff240015e/option1.png" width="50">
+                        </div>
+                      </div>
+                      
+                      <div style="color:#555555;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;line-height:1.5;padding-top:10px;padding-right:25px;padding-bottom:40px;padding-left:25px">
+                        <div style="line-height:1.5;font-size:12px;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;color:#555555">
+                          <p style="text-align:center;line-height:1.5;word-break:break-word;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;margin:0"><strong><span style="color:#000000"><span style="font-size:16px">{ADMIN_NAME} picks questions</span></span></strong></p>
+                          <p style="text-align:center;line-height:1.5;word-break:break-word;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;margin:0"><span style="color:#000000"><span style="font-size:16px">every month.</span></span></p>
+                        </div>
+                      </div>
+                      
+                      <div style="font-size:16px;text-align:center;font-family:Arial,Helvetica Neue,Helvetica,sans-serif">
+                        <div>
+                          <img src="https://ucarecdn.com/23aa7d36-5952-47b2-9aa5-0f1b3679315e/" width="50">
+                        </div>
+                      </div>
+                      
+                      <div style="color:#555555;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;line-height:1.5;padding-top:10px;padding-right:25px;padding-bottom:40px;padding-left:25px">
+                        <div style="line-height:1.5;font-size:12px;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;color:#555555">
+                          <p style="line-height:1.5;word-break:break-word;text-align:center;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;margin:0"><span style="color:#000000"><span style="font-size:16px"><strong>You reply via email —</strong> </span></span></p>
+                          <p style="line-height:1.5;word-break:break-word;text-align:center;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;margin:0"><span style="color:#000000"><span style="font-size:16px">it only takes a few minutes!</span></span></p>
+                        </div>
+                      </div>
+                      
+                      <div style="font-size:16px;text-align:center;font-family:Arial,Helvetica Neue,Helvetica,sans-serif">
+                        <div>
+                          <img src="https://ucarecdn.com/4594c21e-e0d5-46fc-8a16-c07c8265d5a2/" width="50">
+                        </div>
+                      </div>
+                      
+                      <div style="color:#555555;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;line-height:1.5;padding-top:10px;padding-right:25px;padding-bottom:50px;padding-left:25px">
+                        <div style="line-height:1.5;font-size:12px;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;color:#555555">
+                          <p style="line-height:1.5;word-break:break-word;text-align:center;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;margin:0"><span style="color:#000000"><span style="font-size:16px"><strong>We send everyone a newsletter</strong> </span></span></p>
+                          <p style="line-height:1.5;word-break:break-word;text-align:center;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;margin:0"><span style="color:#000000"><span style="font-size:16px">with all the replies.</span></span></p>
+                        </div>
+                      </div>
+                      
+                      <table border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout:fixed;vertical-align:top;border-spacing:0;border-collapse:collapse;min-width:100%" role="presentation" valign="top">
+                        <tbody>
+                          <tr style="vertical-align:top" valign="top">
+                            <td style="word-break:break-word;vertical-align:top;min-width:100%;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px" valign="top">
+                              <table border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout:fixed;vertical-align:top;border-spacing:0;border-collapse:collapse;border-top:2px solid #eaeaea;width:100%" align="center" role="presentation" valign="top">
+                                <tbody>
+                                  <tr style="vertical-align:top" valign="top">
+                                    <td style="word-break:break-word;vertical-align:top;height:0;line-height:0" valign="top"><span></span></td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                      
+                    </div>
+                    
+                  </div>
+                </div>
+                
+                
+              </div>
+            </div>
+          </div>
+          <div style="background-color:transparent">
+            <div class="m_-9144213743796713562block-grid" style="Margin:0 auto;min-width:320px;max-width:500px;word-wrap:break-word;word-break:break-word;background-color:transparent">
+              <div style="border-collapse:collapse;display:table;width:100%;background-color:transparent">
+                
+                
+                <div class="m_-9144213743796713562col" style="min-width:320px;max-width:500px;display:table-cell;vertical-align:top;width:500px">
+                  <div style="width:100%!important">
+                    
+                    <div style="border-top:0px solid transparent;border-left:0px solid transparent;border-bottom:0px solid transparent;border-right:0px solid transparent;padding-top:5px;padding-bottom:5px;padding-right:0px;padding-left:0px">
+                      
+                      <div style="color:#393d47;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;line-height:1.2;padding-top:10px;padding-right:25px;padding-bottom:10px;padding-left:25px">
+                        <div style="line-height:1.2;font-size:12px;color:#393d47;font-family:Arial,Helvetica Neue,Helvetica,sans-serif">
+                          <p style="text-align:center;line-height:1.2;word-break:break-word;margin:0">Got questions? Want to share feedback? Need to make changes?</p>
+                          <p style="text-align:center;line-height:1.2;word-break:break-word;margin:0"><br>Reach us at: <a href="mailto:looping@gmail.com" target="_blank">looping@gmail.com</a></p>
+                        </div>
+                      </div>
+                      
+                      
+                    </div>
+                    
+                  </div>
+                </div>
+                
+                
+              </div>
+            </div>
+          </div>
+          
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  
+  </div>
+  
+"""
